58efb4b0beedbfc879c551f8403b4d80f16cf7fc
Andy Bond <andybond87@gmail.com>
2017-11-03 07:29:07 +0000

    Additional commenting and reformatting

--- happy_vcfeval/forms.py
+++ happy_vcfeval/forms.py
@@ -1,21 +1,28 @@
 from django import forms
 
+
 class upload_form(forms.Form):
-	email = forms.EmailField(label="Email Address")
-	vcf_file = forms.FileField(label="Attach VCF (.vcf) or gzipped VCF (.vcf.gz)")
-	bed_file = forms.FileField(label="Attach analysis region BED (.bed) (OPTIONAL)", required=False)
-	def clean(self):
-		cleaned_data = super(upload_form, self).clean()
-		uploaded_vcf = cleaned_data.get("vcf_file")
-		uploaded_bed = cleaned_data.get("bed_file")
-		if uploaded_vcf._size > 200000000:
-			raise forms.ValidationError({'vcf_file':"File size cannot be greater than 200MB"})
-		#if uploaded_vcf.content_type != "application/gzip":
-		#	raise forms.ValidationError({'vcf_file':"File must be of type 'gzip'. Please upload a gzipped VCF (.vcf.gz)."})
-		if not uploaded_vcf.name.endswith(".vcf.gz") and not uploaded_vcf.name.endswith(".vcf"):
-			raise forms.ValidationError({'vcf_file':"File must be VCF (.vcf) or gzipped VCF ('.vcf.gz')"})
-		if uploaded_bed: #Check if bed uploaded
-			if uploaded_bed._size > 200000000:
-				raise forms.ValidationError({'bed_file':"File size cannot be greater than 200MB"})
-			if not uploaded_bed.name.endswith(".bed"):
-				raise forms.ValidationError({'bed_file':"File must be BED file with '.bed' extension"})			
+    # Define fields
+    email = forms.EmailField(label="Email Address")
+    vcf_file = forms.FileField(label="Attach VCF (.vcf) or gzipped VCF (.vcf.gz)")
+    bed_file = forms.FileField(label="Attach analysis region BED (.bed) (OPTIONAL)", required=False)
+
+    def clean(self):
+        # Validate submitted data
+        cleaned_data = super(upload_form, self).clean()
+        uploaded_vcf = cleaned_data.get("vcf_file")
+        uploaded_bed = cleaned_data.get("bed_file")
+        # Check uploaded vcf size does not exceed 200MB
+        if uploaded_vcf._size > 200000000:
+            raise forms.ValidationError({'vcf_file': "File size cannot be greater than 200MB"})
+        # Check uploaded file has .vcf or .vcf.gz extension
+        if not uploaded_vcf.name.endswith(".vcf.gz") and not uploaded_vcf.name.endswith(".vcf"):
+            raise forms.ValidationError({'vcf_file': "File must be VCF (.vcf) or gzipped VCF ('.vcf.gz')"})
+        # If a bed file has been uploaded...
+        if uploaded_bed:
+            # Check uploaded bed size does not exceed 200MB
+            if uploaded_bed._size > 200000000:
+                raise forms.ValidationError({'bed_file': "File size cannot be greater than 200MB"})
+            # Check uploaded file has .bed extension
+            if not uploaded_bed.name.endswith(".bed"):
+                raise forms.ValidationError({'bed_file': "File must be BED file with '.bed' extension"})
--- happy_vcfeval/views.py
+++ happy_vcfeval/views.py
@@ -7,62 +7,77 @@ import os
 import re
 import threading
 import time
-from precision_medicine import *
+from precision_medicine import upload2Nexus
 
-# upload to nexus and run app
+
+# Upload to nexus and run app
 def run_prec_med(email, vcf_filepath, bed_filepath):
-    upload=upload2Nexus()
+    upload = upload2Nexus()
     upload.take_inputs(email, vcf_filepath, bed_filepath)
 
-# main upload page
+
+# Main upload page
 def upload(request):
+    # Create list of any issues in known_issues.txt so that they can be displayed on page
     with open(settings.BASE_DIR + '/known_issues.txt', 'r') as f:
         known_issues = [x for x in f if not x.startswith("#")]
+    # If data has been submitted...
     if request.method == 'POST':
-        form = upload_form(request.POST, request.FILES) #Pass posted data to form object for validation
-        if form.is_valid(): # if submitted data passes validation
+        # Pass posted data to form upload_form object for validation
+        form = upload_form(request.POST, request.FILES)
+        if form.is_valid():  # if submitted data passes validation
+            # Capture the posted data
             email = request.POST["email"]
             vcf_file = request.FILES["vcf_file"]
+            # Create timestamp
             timestamp = time.strftime("%y%m%d_%H%M%S")
-            # Save the uploaded file(s), and remove any illegal characters or whitespace from filename
+            # Save the uploaded vcf file with original filename
             fs = FileSystemStorage(settings.MEDIA_ROOT + timestamp, settings.MEDIA_URL + timestamp)
             vcf_filename_orig = fs.save(vcf_file.name, vcf_file)
             vcf_filepath_orig = settings.MEDIA_ROOT + timestamp + "/" + vcf_filename_orig
-            #Replace any whitespace/special characters in filename with underscores
+            # Replace any whitespace/special characters in filename with underscores
             vcf_filename = re.sub('[^0-9a-zA-Z.\-/]+', '_', vcf_filename_orig)
             vcf_filepath = settings.MEDIA_ROOT + timestamp + "/" + vcf_filename
             os.rename(vcf_filepath_orig, vcf_filepath)
-            # if user supplied bed file...           
+            # If user supplied bed file...
             if "bed_file" in request.FILES:
+                # Save the uploaded bed file with original file name
                 bed_file = request.FILES["bed_file"]
                 bed_filename_orig = fs.save(bed_file.name, bed_file)
                 bed_filepath_orig = settings.MEDIA_ROOT + timestamp + "/" + bed_filename_orig
-                #Replace any whitespace/special characters in filename with underscores
+                # Replace any whitespace/special characters in filename with underscores
                 bed_filename = re.sub('[^0-9a-zA-Z.\-/]+', '_', bed_filename_orig)
                 bed_filepath = settings.MEDIA_ROOT + timestamp + "/" + bed_filename
                 os.rename(bed_filepath_orig, bed_filepath)
-                # run dna_nexus in background thread
-                t = threading.Thread(target=run_prec_med, kwargs={'email': email, 'vcf_filepath': vcf_filepath, 'bed_filepath': bed_filepath})
-            else: # no user supplied bedfile...
-                t = threading.Thread(target=run_prec_med, kwargs={'email': email, 'vcf_filepath': vcf_filepath, 'bed_filepath': ""})
-            t.setDaemon(True)
-            t.start()
+                # Run dna_nexus in background thread. Calls run_prec_med() function.
+                t = threading.Thread(target=run_prec_med, kwargs={'email': email, 'vcf_filepath': vcf_filepath,
+                                                                  'bed_filepath': bed_filepath})
+            # if user did not supply bedfile...
+            else:
+                # Run dna_nexus in background thread. Calls run_prec_med(), passing empty string as bed_filepath
+                t = threading.Thread(target=run_prec_med, kwargs={'email': email, 'vcf_filepath': vcf_filepath,
+                                                                  'bed_filepath': ""})
+            t.setDaemon(True)  # Run in background
+            t.start()  # Start the thread, which calls run_prec_med() function
+            # Redirect to the 'processing' success page.
             return redirect(reverse('happy_vcfeval:processing'))
         else:
             # If validation failed, reload the form which will display the error messages from failed validation
-            return render(request, 'happy_vcfeval/upload.html', {'form': form, 
-                                                                 'known_issues': known_issues, 
+            return render(request, 'happy_vcfeval/upload.html', {'form': form,
+                                                                 'known_issues': known_issues,
                                                                  'happy_version': happy_version,
                                                                  'na12878_fastq': settings.MEDIA_URL + "FASTQ/NA12878_WES.zip",
                                                                  'our_results': settings.MEDIA_URL + "170624_184727/170624_184727.tar.gz"})
+    # If data hasn't been submitted, just display the webpage
     else:
-    	form = upload_form()
-    	return render(request, 'happy_vcfeval/upload.html', {'form': form, 
+        form = upload_form()
+        return render(request, 'happy_vcfeval/upload.html', {'form': form,
                                                              'known_issues': known_issues,
-                                                             'happy_version': happy_version, 
+                                                             'happy_version': happy_version,
                                                              'na12878_fastq': settings.MEDIA_URL + "FASTQ/NA12878_WES.zip",
                                                              'our_results': settings.MEDIA_URL + "170624_184727/170624_184727.tar.gz"})
 
+
 # page displayed to inform user file is being processed
 def processing(request):
-	return render(request, 'happy_vcfeval/processing.html')
+    return render(request, 'happy_vcfeval/processing.html')
